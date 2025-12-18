719cff19a74d7c86a8b22e7118043f34de04d45c
dimas <dimaswh19@gmail.com>
2025-12-18 20:40:29 +0700

    Menambahkan file DestinasiEntry.kt untuk rute navigasi entri siswa

---
+++ app/src/main/java/com/example/questapi_078/uicontroller/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.questapi_078.uicontroller.route
+
+import com.example.questapi_078.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
